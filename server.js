117e093cd2bfb6adbf8e0a86a84fed57ee9a0be4
reyan <2001103609@student.buksu.edu.ph>
2023-07-02 22:52:14 +0800

    added db connection

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const PORT = process.env.PORT || 3001;
 const DB = process.env.DB || 'mongodb://0.0.0.0:27017/agriMap';
 // const { config } = require('process');
 
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 // app.use(config);
 
+
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('Connected to DB');
+});
 
 app.listen(PORT, () => {
     console.log(`started @ http://localhost:${PORT}/`);
